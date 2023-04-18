f5ae3bda9d9e6919f0086a1a9ada839ccd2b455b
Adam Östberg <adamost03@gmail.com>
2023-04-18 20:57:20 +0200

    La till fiende setup

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 
-mod movement;
 mod a_star;
+mod movement;
 
-use movement::{select_system, move_system};
+use movement::{move_system, select_system};
 
 fn main() {
     App::new()
@@ -16,6 +16,33 @@ fn main() {
 
 const TILE_SIZE: f32 = 20.0;
 
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+    commands
+        .spawn(MaterialMesh2dBundle {
+            mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
+            transform: Transform::default().with_scale(Vec3::splat(TILE_SIZE)),
+            material: materials.add(ColorMaterial::from(Color::PURPLE)),
+            ..default()
+        })
+        .insert(Character);
+
+    commands
+        .spawn(MaterialMesh2dBundle {
+            mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
+            transform: Transform::from_translation(Vec3::new(100., 0., 0.))
+                .with_scale(Vec3::splat(TILE_SIZE)),
+            material: materials.add(ColorMaterial::from(Color::RED)),
+            ..default()
+        })
+        .insert(Enemy)
+        .insert(Health(10));
+}
+
 #[derive(Component)]
 struct Character;
 
@@ -25,12 +52,17 @@ struct Selected;
 #[derive(Component)]
 struct Marker;
 
-fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
-    commands.spawn(Camera2dBundle::default());
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
-        transform: Transform::default().with_scale(Vec3::splat(TILE_SIZE)),
-        material: materials.add(ColorMaterial::from(Color::PURPLE)),
-        ..default()
-    }).insert(Character);
-}
+#[derive(Component)]
+struct Enemy;
+
+#[derive(Component)]
+struct Health(u32);
+
+#[derive(Component)]
+struct Active;
+
+#[derive(Component)]
+struct Attacker;
+
+#[derive(Component)]
+struct Defender;
--- src/movement.rs
+++ src/movement.rs
@@ -1,22 +1,41 @@
+use crate::{Character, Marker, Selected, TILE_SIZE};
 use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
-use crate::{Selected, Character, Marker, TILE_SIZE};
 
-pub (crate) fn select_system(windows: Query<&Window>, cameras: Query<(&Camera, &GlobalTransform)>, characters: Query<(&Transform, Entity), (With<Character>, Without<Selected>)>, selected: Query<Entity, With<Selected>>, mut commands: Commands, buttons: Res<Input<MouseButton>>, mut materials: ResMut<Assets<ColorMaterial>>) {
+pub(crate) fn select_system(
+    windows: Query<&Window>,
+    cameras: Query<(&Camera, &GlobalTransform)>,
+    characters: Query<(&Transform, Entity), (With<Character>, Without<Selected>)>,
+    selected: Query<Entity, With<Selected>>,
+    mut commands: Commands,
+    buttons: Res<Input<MouseButton>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
     let window = windows.single();
     let (camera, camera_transform) = cameras.single();
 
     let mut hover: Option<Entity> = None;
 
-    if let Some(mouse_position) = window.cursor_position()
+    if let Some(mouse_position) = window
+        .cursor_position()
         .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
-        .map(|ray| ray.origin.truncate()) {
+        .map(|ray| ray.origin.truncate())
+    {
         for (transform, entity) in characters.iter() {
-            if transform.translation.x - TILE_SIZE / 2.0 < mouse_position.x && transform.translation.x + TILE_SIZE / 2.0 > mouse_position.x && transform.translation.y - TILE_SIZE / 2.0 < mouse_position.y && transform.translation.y + TILE_SIZE / 2.0 > mouse_position.y {
+            if transform.translation.x - TILE_SIZE / 2.0 < mouse_position.x
+                && transform.translation.x + TILE_SIZE / 2.0 > mouse_position.x
+                && transform.translation.y - TILE_SIZE / 2.0 < mouse_position.y
+                && transform.translation.y + TILE_SIZE / 2.0 > mouse_position.y
+            {
                 hover = Some(entity);
-                commands.entity(entity).remove::<Handle<ColorMaterial>>().insert(materials.add(ColorMaterial::from(Color::GREEN)));
-            }
-            else {
-                commands.entity(entity).remove::<Handle<ColorMaterial>>().insert(materials.add(ColorMaterial::from(Color::PURPLE)));
+                commands
+                    .entity(entity)
+                    .remove::<Handle<ColorMaterial>>()
+                    .insert(materials.add(ColorMaterial::from(Color::GREEN)));
+            } else {
+                commands
+                    .entity(entity)
+                    .remove::<Handle<ColorMaterial>>()
+                    .insert(materials.add(ColorMaterial::from(Color::PURPLE)));
             }
         }
     }
@@ -24,42 +43,62 @@ pub (crate) fn select_system(windows: Query<&Window>, cameras: Query<(&Camera, &
     if buttons.just_pressed(MouseButton::Left) {
         if let Some(entity) = hover {
             commands.entity(entity).insert(Selected);
-            commands.entity(entity).remove::<Handle<ColorMaterial>>().insert(materials.add(ColorMaterial::from(Color::BLUE)));
-        }
-        else {
+            commands
+                .entity(entity)
+                .remove::<Handle<ColorMaterial>>()
+                .insert(materials.add(ColorMaterial::from(Color::BLUE)));
+        } else {
             for entity in selected.iter() {
                 commands.entity(entity).remove::<Selected>();
-                commands.entity(entity).remove::<Handle<ColorMaterial>>().insert(materials.add(ColorMaterial::from(Color::PURPLE)));
+                commands
+                    .entity(entity)
+                    .remove::<Handle<ColorMaterial>>()
+                    .insert(materials.add(ColorMaterial::from(Color::PURPLE)));
             }
         }
     }
 }
 
-pub (crate) fn move_system(mut selected: Query<&mut Transform, With<Selected>>, mut markers: Query<(&mut Transform, Entity), (With<Marker>, Without<Selected>)>, windows: Query<&Window>, cameras: Query<(&Camera, &GlobalTransform)>, buttons: Res<Input<MouseButton>>, mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
+pub(crate) fn move_system(
+    mut selected: Query<&mut Transform, With<Selected>>,
+    mut markers: Query<(&mut Transform, Entity), (With<Marker>, Without<Selected>)>,
+    windows: Query<&Window>,
+    cameras: Query<(&Camera, &GlobalTransform)>,
+    buttons: Res<Input<MouseButton>>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
     let window = windows.single();
     let (camera, camera_transform) = cameras.single();
 
     if let Some(mut transform) = selected.iter_mut().next() {
         if let Some(mut marker) = markers.iter_mut().next() {
-            if let Some(mouse_position) = window.cursor_position()
+            if let Some(mouse_position) = window
+                .cursor_position()
                 .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
-                .map(|ray| ray.origin.truncate()) {
-                marker.0.translation = Vec3::from((snap(mouse_position.x, 20, 1), snap(mouse_position.y, 20, 1), 0.0));
+                .map(|ray| ray.origin.truncate())
+            {
+                marker.0.translation = Vec3::from((
+                    snap(mouse_position.x, 20, 1),
+                    snap(mouse_position.y, 20, 1),
+                    0.0,
+                ));
                 if buttons.just_pressed(MouseButton::Left) {
                     transform.translation = marker.0.translation;
                 }
             }
+        } else {
+            commands
+                .spawn(MaterialMesh2dBundle {
+                    mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
+                    transform: Transform::default().with_scale(Vec3::splat(TILE_SIZE)),
+                    material: materials.add(ColorMaterial::from(Color::PURPLE)),
+                    ..default()
+                })
+                .insert(Marker);
         }
-        else {
-            commands.spawn(MaterialMesh2dBundle {
-                mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
-                transform: Transform::default().with_scale(Vec3::splat(TILE_SIZE)),
-                material: materials.add(ColorMaterial::from(Color::PURPLE)),
-                ..default()
-            }).insert(Marker);
-        }
-    }
-    else {
+    } else {
         for (_, marker) in markers.iter() {
             commands.entity(marker).despawn();
         }
@@ -67,5 +106,6 @@ pub (crate) fn move_system(mut selected: Query<&mut Transform, With<Selected>>, 
 }
 
 fn snap(original: f32, numinator: u32, denuminator: u32) -> f32 {
-    (original / numinator as f32 * denuminator as f32).round() * numinator as f32 / denuminator as f32
+    (original / numinator as f32 * denuminator as f32).round() * numinator as f32
+        / denuminator as f32
 }
